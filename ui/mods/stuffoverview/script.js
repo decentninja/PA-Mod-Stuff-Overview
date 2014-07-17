a13d84b82a8eb62775c95919b51a3f9bec56c685
Sootn <Sootn@Svarta-Sara.(none)>
2014-07-17 18:41:34 +0200

    Working

--- ui/mods/stuffoverview/script.js
+++ ui/mods/stuffoverview/script.js
@@ -1,21 +1,40 @@
 function StuffOverview() {
-	var documentid = "sootns_stuff_overview"
+	var documentid = "stuff_overview"
 	createFloatingFrame(documentid, 240, 50, {
 		offset: "topRight",
 		left: -240
 	})
 	this.html = document.querySelector("#" + documentid)
 
-	lib.addHandler("watch_list", this.watch_list)
-	var alertTypes = ['watchlist.setCreationAlertTypes', 'watchlist.setDeathAlertTypes', 'watchlist.setSightAlertTypes', 'watchlist.setTargetDestroyedAlertTypes', 'watchlist.setDamageAlertTypes']
-	var include = ['Mobile', 'Structure', 'Recon']
-	var exclude = []
-	alertTypes.forEach(function(type) {
-		engine.call(type, include, exclude)
-	})
+	this.setupWatchlist()
 }
 
-StuffOverview.prototype.watch_list = function(payload) {
+StuffOverview.prototype.setupWatchlist = function() {
+	console.log("- Stuffoverview setting up watchlist")
+	lib.addHandler("watch_list", this.handleEvent)
+	
+	var alertTypes = [
+		'watchlist.setCreationAlertTypes',
+		'watchlist.setDeathAlertTypes',
+		//'watchlist.setSightAlertTypes',
+		'watchlist.setTargetDestroyedAlertTypes'
+		//'watchlist.setDamageAlertTypes'
+	]
+	var include = ['Mobile', 'Structure', 'Recon']
+	var exclude = []
+	// Need to load after live_game and other mods that set watchlist stuff
+	window.setTimeout(function() {
+		alertTypes.forEach(function(type) {
+			engine.call(
+				type,
+				JSON.stringify(include),
+				JSON.stringify(exclude)
+			)
+		})
+	}, 2000)
+}
+
+StuffOverview.prototype.handleEvent = function(payload) {
 	console.log("blub", payload)
 }
 
